e5ad47c642e665b9a63db3bf8f48ede9bb08b5bc
Peza <152343973+ManzanillaSebastian@users.noreply.github.com>
2025-06-25 20:01:01 +0000

    realizado

--- src/main.js
+++ src/main.js
@@ -23,3 +23,65 @@ const itemData = {
         score: 36
     }
 };
+
+function enableForm(){
+    const select = document.getElementById("items");
+
+    let index = 0;
+
+    Object.values(itemData).forEach(item => {
+        //<option value="-1" disabled>Seleccione un valor</option>
+        let option = document.createElement("option");
+
+        option.value = index;
+        option.textContent = item.name;
+
+        select.appendChild(option);
+
+        index++;
+    });
+
+    select.addEventListener("change", function () {
+        const selectedIndex = parseInt(select.value);
+        mostrarItemSeleccionado(selectedIndex);
+    });
+
+    document.getElementById("increaseScore").addEventListener("click", function () {
+        const items = Object.values(itemData);
+        if (select.value < 0 || select.value >= items.length) return;
+        items[select.value].score++;
+        document.getElementById("score").value = items[select.value].score;
+    });
+
+    // Botón para disminuir el score
+    document.getElementById("decreaseScore").addEventListener("click", function () {
+        const items = Object.values(itemData);
+        if (select.value < 0 || select.value >= items.length) return;
+        items[select.value].score--;
+        document.getElementById("score").value = items[select.value].score;
+    });
+}
+
+function mostrarItemSeleccionado(index) {
+    const items = Object.values(itemData);
+    if (index < 0 || index >= items.length) return;
+
+    const item = items[index];
+    document.getElementById("displayImage").src = item.image;
+    document.getElementById("photographer").value = item.photographer;
+    document.getElementById("description").value = item.description;
+    document.getElementById("score").value = item.score;
+
+    const select = document.getElementById("items");
+
+    select.addEventListener("change", function () {
+        const selectedIndex = parseInt(this.value);
+        mostrarItemSeleccionado(selectedIndex);
+    });
+}
+
+
+
+{{
+    enableForm();
+}}
